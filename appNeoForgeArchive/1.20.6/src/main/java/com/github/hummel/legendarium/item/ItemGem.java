a5329aa6379d8e96bf445d43707a15ae2d33f461
Hummel009 <hummel.turbamentum@gmail.com>
2026-02-01 15:19:44 +0300

    rename & cut support

--- appNeoForgeArchive/1.20.6/src/main/java/com/github/hummel/legendarium/item/ItemGem.java
+++
@@ -1,9 +0,0 @@
-package com.github.hummel.legendarium.item;
-
-import net.minecraft.world.item.Item;
-
-public class ItemGem extends Item {
-	public ItemGem() {
-		super(new Properties().stacksTo(1));
-	}
-}
